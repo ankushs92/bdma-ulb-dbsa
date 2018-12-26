66d4591003342e24a65870ae8460072971d016d4
René Gómez Londoño <renegomezlondoo@gmail.com>
2018-12-26 13:08:53 +0100

    modified memory map read

---
+++ src/main/java/streams/read/MemMapReadStream.java
@@ -0,0 +1,105 @@
+package streams.read;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import streams.interfaces.AbstractReadStream;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class MemMapReadStream implements AbstractReadStream {
+
+    private final static Logger logger = LoggerFactory.getLogger(MemMapReadStream.class);
+
+    private static final String READ_MODE = "r";
+    //All inputs are 32bit Integers UNIT_SIZE
+    private static final Integer UNIT_SIZE = 4;
+    private MappedByteBuffer mappedByteBuffer;
+    private RandomAccessFile randomAccessFile;
+    private FileChannel fileChannel;
+    private long fileSize = 0;
+    private long currentPosition = 0;
+
+
+    private int bufferSize;
+
+    public MemMapReadStream(int bufferSize) {
+        if(bufferSize < UNIT_SIZE){
+            logger.warn("Buffer smaller than UNIT_SIZE. Setting to min read size = " + UNIT_SIZE + " Bytes.");
+            bufferSize = 1; //One unit size
+        }
+        this.bufferSize = bufferSize * UNIT_SIZE;
+    }
+
+    @Override
+    public void open(final String fileLocation) throws IOException {
+        randomAccessFile = new RandomAccessFile(fileLocation, READ_MODE);
+        fileChannel = randomAccessFile.getChannel();
+        fileSize = fileChannel.size();
+        currentPosition = 0;
+        mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, bufferSize);
+    }
+
+    @Override
+    public Integer readNext() {
+        if(currentPosition < 0)
+            return null;
+        Integer readNumber = null;
+        readNumber = mappedByteBuffer.getInt();
+        currentPosition += UNIT_SIZE;
+        if(mappedByteBuffer.remaining() < UNIT_SIZE)
+        {
+            if(currentPosition > fileSize) {
+                currentPosition = -1;
+            }
+            else{
+                mappedByteBuffer.clear();
+                try {
+                    if(bufferSize > fileSize - currentPosition)
+                        bufferSize = (int) (fileSize - currentPosition);
+                    System.out.println("Buffer reload in position " + currentPosition);
+//                    mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, currentPosition, bufferSize);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return readNumber;
+
+    }
+
+    public void close(){
+        try {
+            fileChannel.close();
+            randomAccessFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public boolean endOfStream() {
+        if(currentPosition < 0) {
+            close();
+            return true;
+        }
+        if(currentPosition < fileSize)
+            return false;
+        return true;
+    }
+
+    public long getFileSize() {
+
+        try {
+            return fileSize;
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+}
